3a8760d19a136ac64091457534b50b53ef6cd8e1
Simant Dhakal <Simant.dhakal@gmail.com>
2019-12-12 08:07:01 +0545

    Splash screen

---
+++ app/src/main/java/com/simant/sportfanatic/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.simant.sportfanatic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Map;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // splash screen time out
+    private static int SPLASH_SCREEN_TIMEOUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // status bar color
+        getWindow().setStatusBarColor(Color.parseColor("#2E8B57"));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
